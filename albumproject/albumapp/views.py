533c499399a69b22d2fe4a55de4b5e88567b7617
Jack Markley <jack.markley@gmail.com>
2014-04-14 04:10:44 +0200

    views written, just need the actual htmls

--- albumproject/albumapp/views.py
+++ albumproject/albumapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def front_view(request):
+    return render(request, 'front.html')
+
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def album_view(request, albumid):
+    myalbum = Album.objects.get(pk=albumid)
+    return render(request, 'album.html', {'photos': myalbum.photos.all()})
+
+
+def photo_view(request, photoid):
+    myphoto = Photo.objects.get(pk=photoid)
+    return render(request, 'photo.html', {'photo': myphoto})
+
+
+def tag_view(request, tagid):
+    mytag = Tag.objects.get(pk=tagid)
+    return render(request, 'tag.html', {'photos': mytag.photos.all()})
